ebf63aa4462820b2da8caa60f3573fbd79e0a091
Nikita Visencuk Visenko <cereshnia@gmail.com>
2020-11-05 15:57:15 +0000

    fixed date type conversion issue

--- hamiltonian_solver.jl
+++ hamiltonian_solver.jl
@@ -3,9 +3,9 @@ module Solver
   include("linear_and_quadratic_subroutines.jl")
   include("paths.jl")
   
-  function HamiltonianSolver(nodes_to_perturb::Array{Int64}, tspan::Tuple{Float64,Float64}, save_path::String = default_save_path, ϵ::ComplexF64 = 0.0 + 0.0im)
+  function HamiltonianSolver(tspan::Tuple{Float64,Float64}, save_path::String = default_save_path, ϵ::ComplexF64 = 0.0 + 0.0im, modes_to_perturb::Array{Any} = [])
     save_solution_to = "$ϵ, $save_path" == "" ? save_path : "$main_path/JLD2/$ϵ, $save_path" 
-    rm(save_solution_to, force=true, recursive=true)
+    rm(save_solution_to, force = true, recursive = true)
     mkdir(save_solution_to) 
     cd(save_solution_to) 
     start_time = Dates.now()
@@ -17,17 +17,22 @@ module Solver
     initial[Int64((N_eq-1)/2)+1]=0.0+0.0*im
     initial[Int64((N_eq-1)/2)+2]=0.1+0.0*im
     
-    # Perturb specified nodes by ϵ if ϵ and vector array are passed.
-    if !isempty(nodes_to_perturb) && !ϵ == 0.0 +0.0im
-      for node in nodes_to_perturb
-        initial[nodes + N + 1] += ϵ
+    # Perturb specified nodes by ϵ if ϵ and perturbed mode array are 
+    # passed.
+    if !isempty(modes_to_perturb) && !ϵ == 0.0 +0.0im
+      for mode in modes_to_perturb
+        initial[mode + N + 1] += ϵ
       end
     end
     
-    initial_conditions=SVector{N_eq}(initial)
+    initial_conditions = SVector{N_eq}(initial)
+    
+    # Solving the system using RK4, with specified initial conditions 
+    # and step-size
     prob = ODEProblem(result,initial_conditions,tspan,p)
     solution = solve(prob,RK4(), dt = 1.0*10^(-6), dense = false, saveat = 0.001)
     
+    # Saving final state of the system
     final_state = last(solution)
     open("final_state.txt","a") do io
       for state in final_state
@@ -35,9 +40,10 @@ module Solver
       end
     end
     
+    # Saving solution data for post-processing and displaying run-time
     @save "sol_tf=$(tspan[2]).jld2" solution
     cd(main_path)
-    println("Run time: $(round((Dates.now() - start_time, Minute))) seconds")
+    println("Run time: $(round((Dates.now() - start_time), Minute))")
     return final_state
   end
 end
